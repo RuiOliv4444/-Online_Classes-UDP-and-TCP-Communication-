5ac6df536afb15dcad450158bed1a37e2b257df9
RuiOliv4444 <132606623+RuiOliv4444@users.noreply.github.com>
2024-03-26 15:02:08 +0000

    Update servidor.c
    Já está tudo a dar (TCP e UDP), acho :)

--- servidor.c
+++ servidor.c
@@ -7,7 +7,7 @@
 #include <arpa/inet.h>
 
 #define PORTO_TURMAS 12345 // Porta TCP
-#define PORTO_CONFIG 12347 // Porta UDP
+#define PORTO_CONFIG 12348 // Porta UDP
 #define BUF_SIZE 512
 #define FILENAME "utilizadores.txt"
 #define TAM 30 
@@ -29,9 +29,14 @@ typedef us_list * lista;
 
 lista cria(void);
 void erro(char *msg);
-void login_admin(const char *username, const char *password, int *admin_logado, lista lista_utilizadores);
+void process_client(int client_fd, struct sockaddr_in client_addr);
+void login_user(const char *username, const char *password, int *login, lista lista_utilizadores);
 void udp_server_function();
 void tcp_server_function (int tcp_fd);
+void list_classes(int client_fd);
+void list_subscribed(int client_fd);
+void create_class(int client_fd);
+void send_cont(int client_fd);
 int add_user(const char *name, const char *pass, const char *ro, lista lista_utilizadores);
 void insere_utilizador(lista lista_utilizadores, struct utilizador person);
 void remove_utilizador(lista *lista_utilizadores, char username[TAM]);
@@ -90,7 +95,7 @@ void tcp_server_function (int tcp_fd){
         if (client > 0) {
             if (fork() == 0) {
                 close(tcp_fd);
-                //função para lidar com todos os casos tem de ser inserida aqui
+                process_client(client, client_addr);
                 exit(0);
             }
             close(client);
@@ -98,8 +103,94 @@ void tcp_server_function (int tcp_fd){
     }
 }
 
+void process_client(int client_fd, struct sockaddr_in client_addr) {
+    char buffer[BUF_SIZE];
 
+	lista lista_utilizadores = cria(); // Acho que vamos tere de abrir e fechar o ficheiro de texto a cada iteração para estarmos sempre com a versao mis atualizada do programa,
+									//pois pode acontecer de quando estamos a corre-lo, o admin pode adicionar, ou outra coisa, algum aluno na base de dados, mas por enquanto vou deixar assim
+	ler_ficheiro(lista_utilizadores);
 
+    char request[] = "Para aceder à sua conta, faça o login\n"; //mensagem de aviso para fazer o login
+    write(client_fd, request, strlen(request));
+	int client_logado = 0;
+    while (1) { //ficamos sempre a ler as mensagens do utilizador e a lidar com elas, até a mensagem recebida ser "SAIR"
+        bzero(buffer, BUF_SIZE);
+		char comando[TAM];
+		char arg1[TAM], arg2[TAM];
+		char mensagem_boa[BUF_SIZE];
+		char mensagem_ma[BUF_SIZE];
+        if (read(client_fd, buffer, BUF_SIZE-1) <= 0){
+			erro("Erro ao receber dados TCP");
+			continue;	
+		}
+        buffer[strcspn(buffer, "\n")] = 0; // Remove a mudança de linha "\n"
+
+		sscanf(buffer, "%s %s %s", comando, arg1, arg2); // analisar cada um dos parametros enviados pelo admin 
+
+		if (strcmp(comando, "LOGIN") == 0) {
+			login_user(arg1,arg2,&client_logado, lista_utilizadores);
+
+			if(client_logado > 1){//mensagem de ter conseguido logar
+				strcpy(mensagem_boa, "Login efetuado com sucesso!\n");
+				if (write(client_fd, mensagem_boa, strlen(mensagem_boa)) < 0) erro("Erro ao enviar resposta TCP");
+				memset(mensagem_boa, 0, sizeof(mensagem_boa));
+			}
+			else{//mensagem de não ter conseguido logar
+				strcpy(mensagem_ma, "Dados de acesso inválidos!\n");
+				if (write(client_fd, mensagem_ma, strlen(mensagem_ma)) < 0) erro("Erro ao enviar resposta TCP");
+				memset(mensagem_ma, 0, sizeof(mensagem_ma));
+			}
+		} else if (client_logado > 1) {								// O admin para ter acesso a esta parte vai ter de se logar primeiro, portanto a primeira mensagem dele terá de ser o login e só depois
+																	//realizar uma das operações abaixo
+			if (strcmp(comando, "LIST_CLASSES") == 0) {
+				// Implementar a lógica para ver as classes, vou por apenas funçoes teste, porque nao sei trabalhar com multicast ainda
+				list_classes(client_fd);
+			} else if (strcmp(comando, "LIST_SUBSCRIBED") == 0) {
+				// Implementar a lógica para ver as classes do user, vou por apenas funçoes teste, porque nao sei trabalhar com multicast ainda
+				list_subscribed(client_fd);
+			} else if (strcmp(comando, "SUSBCRIBE_CLASS") == 0) {
+				// Implementar a lógica para subscrever uma aula, vou por apenas funçoes teste, porque nao sei trabalhar com multicast ainda
+
+			} else if (strcmp(comando, "DISCONNECT") == 0) {
+				escrever_ficheiro(lista_utilizadores);
+				close(client_fd);
+				exit(0);
+			}
+		}else if(client_logado == 3){
+			if (strcmp(comando, "CREATE_CLASS") == 0) {
+				// Implementar a lógica para ver criar uma turma, vou por apenas funçoes teste, porque nao sei trabalhar com multicast ainda
+				create_class(client_fd);
+			} else if (strcmp(comando, "SEND") == 0) {
+				// Implementar a lógica para listar as turmas, vou por apenas funçoes teste, porque nao sei trabalhar com multicast ainda
+				send_cont(client_fd);
+			}		
+		}else {
+			if (write(client_fd, "É necessário efetuar login primeiro!\n", strlen("É necessário efetuar login primeiro!\n")) < 0) erro("Erro ao enviar resposta TCP");// Responde que é necessário fazer login primeiro.
+		}
+		memset(buffer, 0, BUF_SIZE); // Prepara para a próxima mensagem.
+	}
+    close(client_fd);// no fim fechamos sempre o socket e o ficheiro
+}
+
+void list_classes(int client_fd) {
+    char message[] = "Esta é uma função protótipo para listar as classes.\n";
+    write(client_fd, message, strlen(message));
+}
+
+void list_subscribed(int client_fd) {
+    char message[] = "Esta é uma função protótipo para listar as classes em que participas.\n";
+    write(client_fd, message, strlen(message));
+}
+
+void create_class(int client_fd) {
+    char message[] = "Esta é uma função protótipo para criar uma aula.\n";
+    write(client_fd, message, strlen(message));
+}
+
+void send_cont(int client_fd) {
+    char message[] = "Esta é uma função protótipo para enviar conteúdo para uma aula.\n";
+    write(client_fd, message, strlen(message));
+}
 //-------------------------DIVISÓRIA DAS FUNÇÔES PERTENCENTES A UDP COMEÇA AQUI----------------------------------
 
 
@@ -132,6 +223,7 @@ void udp_server_function() {
 
 			char mensagem_boa[BUF_SIZE];
 			char mensagem_ma[BUF_SIZE];
+			memset(buf, 0, BUF_SIZE); 
 			if (recvfrom(udp_fd, buf, BUF_SIZE, 0, (struct sockaddr*)&client_addr, &addrlen) < 0){
 				erro("Erro ao receber dados UDP");
 				continue;											// ter a certeza 
@@ -139,11 +231,13 @@ void udp_server_function() {
 			if (strlen(buf) == 0) {									// Nenhum dado foi recebido, passar à frente, evitamos que envia a mensagem de pedir para iniciar sessão
 				continue;
 			}
-			sscanf(buf, "%s %s %s %s", comando, arg1, arg2, arg3); // analisar cada um dos parametros enviados pelo admin 
-
+			if (sscanf(buf, "%s %s %s", comando, arg1, arg2, arg3) >= 1) {
+   				 printf("Command: %s, Arg1: %s, Arg2: %s\n", comando, arg1, arg2);
+			}
+			
 			if (strcmp(comando, "LOGIN") == 0) {
 
-				login_admin(arg1,arg2,&admin_logado, lista_utilizadores);
+				login_user(arg1,arg2,&admin_logado, lista_utilizadores);
 
 				if(admin_logado == 1){//mensagem de ter conseguido logar
 					strcpy(mensagem_boa, "Login efetuado com sucesso!\n");
@@ -151,11 +245,12 @@ void udp_server_function() {
 					memset(mensagem_boa, 0, sizeof(mensagem_boa));
 				}
 				else{//mensagem de não ter conseguido logar
+					printf("Mensagem que está em cada string: %s // %s // %s",comando, arg1, arg2 );
 					strcpy(mensagem_ma, "Dados de acesso inválidos!\n");
 					if (sendto(udp_fd,mensagem_ma, strlen(mensagem_ma), 0, (struct sockaddr*)&client_addr, addrlen) < 0) erro("Erro ao enviar resposta UDP");
 					memset(mensagem_ma, 0, sizeof(mensagem_ma));
 				}
-			} else if (admin_logado) {								// O admin para ter acesso a esta parte vai ter de se logar primeiro, portanto a primeira mensagem dele terá de ser o login e só depois
+			} else if (admin_logado == 1) {								// O admin para ter acesso a esta parte vai ter de se logar primeiro, portanto a primeira mensagem dele terá de ser o login e só depois
 																	//realizar uma das operações abaixo
 				if (strcmp(comando, "ADD_USER") == 0) {
 					if(add_user(arg1, arg2, arg3,lista_utilizadores)){
@@ -188,20 +283,23 @@ void udp_server_function() {
     }
 }
 
-void login_admin(const char *username, const char *password, int *admin_logado, lista lista_utilizadores) {//verifica se a pessoa que está a dar login está a por os dados corretos e
-																											//e se é mesmo um administrador
-    lista atual = lista_utilizadores->next; // Supondo que o primeiro nó seja um cabeçalho/dummy
+void login_user(const char *username, const char *password, int *login, lista lista_utilizadores) {//verifica se a pessoa que está a dar login está a por os dados corretos e
+    *login = 0;
+	lista atual = lista_utilizadores->next;
     while (atual != NULL) {
         if (strcmp(username, atual->user.username) == 0 && strcmp(password, atual->user.password) == 0) {
             if (strcmp(atual->user.role, "administrador") == 0) {
-                *admin_logado = 1;
-                return; // Encontrou um administrador com as credenciais fornecidas
-            }
+                *login = 1;
+				return;
+            }else if(strcmp(atual->user.role, "aluno") == 0){
+				*login = 2; 
+				return;
+			}else if(strcmp(atual->user.role, "professor") == 0)
+				*login = 3; 
+				return;
         }
         atual = atual->next; // Move para o próximo nó
     }
-
-    *admin_logado = 0; // Não encontrou um administrador com as credenciais fornecidas
 }
 int add_user(const char *name, const char *pass, const char *ro, lista lista_utilizadores) {  			// admin adiciona um usuário
 	int result = 1;
@@ -229,7 +327,7 @@ int verifica_func(const char aux[TAM]){																// verifica se estamos a 
 
     return count;
 }
-//------------------------- FUNÇÕES RESPONSAVEIS PELO TRATAMENTO DOS DADOS ----------------------------------
+//------------------------- FUNÇÕES RESPONSAVEIS PELO TRATAMENTO DA LISTA DE UTILIZADORES ----------------------------------
 
 
 lista cria (){  //Funcao que cria uma lista com 1 elemento e retorna essa mesma lista
@@ -288,7 +386,7 @@ void listar_users(lista lista_utilizadores,int udp_fd,struct sockaddr_in client_
 }
 
 
-
+//------------------------- FUNÇÕES RESPONSAVEIS PELO TRATAMENTO DO FICHERIO DE TEXTO ----------------------------------
 
 
 void ler_ficheiro(lista lista_utilizadores) {
