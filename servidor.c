c4ad0e610af397bf987527aa34fd2304328aadc8
marianarbsousa <133211553+marianarbsousa@users.noreply.github.com>
2024-03-30 12:06:51 +0000

    Add files via upload

--- servidor.c
+++ servidor.c
@@ -1,65 +1,6 @@
-#include <stdio.h>
+#include "servidor.h"
 #include <stdlib.h>
 #include <string.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <netdb.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <sys/socket.h>
-#include <sys/shm.h>
-#include <signal.h>
-#include <semaphore.h>
-#include <sys/ipc.h>
-#include <errno.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <time.h>
-
-#define BUF_SIZE 512
-#define TAM 30 
-
-FILE *file;
-char *filename;
-int tcp_fd, udp_fd;
-
-struct utilizador {
-    char username[TAM];
-    char password[TAM];
-    char role[TAM];
-};
-
-typedef struct us_list { //Struct da lista ligada
-    struct utilizador user;
-    struct us_list * next;
-} us_list;
-
-typedef us_list * lista;
-
-lista cria(void);
-void erro(char *msg);
-void process_client(int client_fd,struct sockaddr_in client_addr, lista lista_utilizadores);
-void login_user(const char *username, const char *password, int *login, lista lista_utilizadores);
-void udp_server_function(unsigned short udp_port,lista lista_utilizadores);
-void tcp_server_function (int tcp_fd, lista lista_utilizadores);
-void list_classes(int client_fd);
-void list_subscribed(int client_fd);
-void subscribe_class(int client_fd);
-void create_class(int client_fd);
-void send_cont(int client_fd);
-int add_user(const char *name, const char *pass, const char *ro, lista lista_utilizadores);
-void insere_utilizador(lista lista_utilizadores, struct utilizador person);
-int remove_utilizador(lista *lista_utilizadores, char username[TAM],struct sockaddr_in client_addr, socklen_t addrlen);
-void ler_ficheiro(lista lista_utilizadores);
-void escrever_ficheiro(lista lista_utilizadores);
-void listar_users(lista lista_utilizadores,int udp_fd,struct sockaddr_in client_addr, socklen_t addrlen);
-int verifica_func(const char aux[TAM]);
-void treat_signal(int sig);
 
 int main(int argc, char *argv[]) {
 	if (argc != 4) {
@@ -150,31 +91,52 @@ void process_client(int client_fd, struct sockaddr_in client_addr, lista lista_u
 			else{//mensagem de não ter conseguido logar
 				if (write(client_fd, "REJECTED!\n", strlen("REJECTED!\n")) < 0) erro("Erro ao enviar resposta TCP");
 			}
-		} else if (client_logado > 1) {								// O admin para ter acesso a esta parte vai ter de se logar primeiro, portanto a primeira mensagem dele terá de ser o login e só depois
+		} 
+		else if (client_logado > 1) {								// O admin para ter acesso a esta parte vai ter de se logar primeiro, portanto a primeira mensagem dele terá de ser o login e só depois
 																	//realizar uma das operações abaixo
 			if (strcmp(comando, "LIST_CLASSES") == 0) {
 				list_classes(client_fd);
 
-			} else if (strcmp(comando, "LIST_SUBSCRIBED") == 0) {
+			} 
+			else if (strcmp(comando, "LIST_SUBSCRIBED") == 0) {
 				list_subscribed(client_fd);
 				
-			} else if (strcmp(comando, "SUSBCRIBE_CLASS") == 0) {
+			} 
+			else if (strcmp(comando, "SUSBCRIBE_CLASS") == 0) {
 				subscribe_class(client_fd);
 
-			} else if (strcmp(comando, "DISCONNECT") == 0) {
+			} 
+			else if (strcmp(comando, "DISCONNECT") == 0) {
 				escrever_ficheiro(lista_utilizadores);
 				close(client_fd);
 				break;
-			}
 			
-		}else if(client_logado == 3){
-			if (strcmp(comando, "CREATE_CLASS") == 0) {
-				create_class(client_fd);
-
-			} else if (strcmp(comando, "SEND") == 0) {
-				send_cont(client_fd);
-			}		
-		}else {
+			}
+			else if (strcmp(comando, "CREATE_CLASS") == 0) {
+				if(client_logado == 3){
+					create_class(client_fd);
+				}
+				else{
+					char message[] = "Comando desconhecido.\n";
+   					write(client_fd, message, strlen(message));
+				}
+			}
+			else if (strcmp(comando, "SEND") == 0) {
+				if(client_logado == 3){
+					send_cont(client_fd);
+				}
+				else{
+					char message[] = "Comando desconhecido.\n";
+   					write(client_fd, message, strlen(message));
+				}
+				
+			}
+			else{
+				char message[] = "Comando desconhecido.\n";
+   				write(client_fd, message, strlen(message));
+			}	
+		}
+		else {
 			if (write(client_fd, "É necessário efetuar login primeiro!\n", strlen("É necessário efetuar login primeiro!\n")) < 0) erro("Erro ao enviar resposta TCP");// Responde que é necessário fazer login primeiro.
 		}
 		memset(buffer, 0, BUF_SIZE); // Prepara para a próxima mensagem.
